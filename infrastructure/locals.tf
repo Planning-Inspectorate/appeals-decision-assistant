c6bbd826914eb724aa3de51ea832e24d042b772f
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-11-27 14:12:42 +0000

    feat(infrastructure): use west-europe for available LLMs

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  org              = "pins"
-  service_name     = "ada"
-  primary_location = "uk-south"
+  org          = "pins"
+  service_name = "ada"
+  # for now we are using west-europe as that is where the models are available
+  primary_location = "eu-west"
 
   resource_suffix         = "${local.service_name}-${var.environment}"
   shorter_resource_suffix = var.environment == "training" ? "${local.service_name}trai" : local.resource_suffix
